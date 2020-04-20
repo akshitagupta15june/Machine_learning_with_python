a27108ccdf3e3998744fdc5550d78e04272cd369
akshitagupta15june <akshitagupta15june@gmail.com>
2020-04-20 18:23:03 +0530

    rotate left+right list

--- rotate_left_prog.py
+++ rotate_left_prog.py
@@ -2,9 +2,12 @@
 def rotate_left(n,m):
     return n[m:]+n[:m]
 
-
+def rotate_right(n,m):
+    return n[-m:]+n[:-m]
 n=[2,5,3,2,6,7,8]
 q=rotate_left(n,2)
+p=rotate_right(n,2)
 print(q)
+print(p)
 
 
